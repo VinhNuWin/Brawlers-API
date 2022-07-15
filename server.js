565af9534f420a790296b79cc818676b365cb954
Vinhthere <105720616+Vinhthere@users.noreply.github.com>
2022-07-15 15:48:55 -0700

    working brawler api

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const brawlers = {
+    'frank':{
+        'health': 7000,
+        'attDmg': 1240,
+        'attRange': 6,
+        'reloadSpeed': '.8s',
+        'moveSpeed': 770
+    },
+    'el primo':{
+        'health': 6000,
+        'attDmg': 1440,
+        'attRange': 3,
+        'reloadSpeed': '.8s',
+        'moveSpeed': 770
+    }, 
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name',(request,response)=>{
+    const brawlerName = request.params.name.toLowerCase()
+    if(brawlers[brawlerName]){
+        response.json(brawlers[brawlerName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Time to Brawl!`)
+})
